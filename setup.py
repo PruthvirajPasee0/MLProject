2ad8de0c9346679e3a84de89f86dff9f0001f16c
PruthvirajPasee0 <pruthvirajpasi676@gmail.com>
2025-01-23 11:29:20 +0530

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = "--"
+HYPEN_E_DOT = "-e ."
 def get_requirements(file_path:str)-> List[str]:
     requirements = []
     with open(file_path) as file_obj:
@@ -17,6 +17,7 @@ setup(
     name= "mlproject",
     version= '0.0.1',
     author="PruthvirajPasee",
+    author_email='pruthvirajpasi42@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
